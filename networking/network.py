6a31de870a7252ddab3a88f1403de68454d2f1cc
cvsae <33999023+cvsae@users.noreply.github.com>
2018-08-02 09:52:16 +0000

    Update network.py

--- networking/network.py
+++ networking/network.py
@@ -20,6 +20,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
+from collections import OrderedDict
 from dboperations import *
 from bootstrap_nodes import *
 import version 
