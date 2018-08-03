f7ef83d45e9da78d75f801355b410baf7c61b09c
cvsae <33999023+cvsae@users.noreply.github.com>
2018-08-03 12:22:01 +0000

    Update bootstrap_nodes.py

--- bootstrap_nodes.py
+++ bootstrap_nodes.py
@@ -1,4 +1,1 @@
-BOOTSTRAP_NODES = ["localhost:1919",
-                   "localhost:5007",
-                   "localhost:5006",
-                   "localhost:5005"]
+BOOTSTRAP_NODES = ["localhost:1919"]
