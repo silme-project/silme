d13bc5f03804475cdae3af69434bdbb465944284
cvsae <33999023+cvsae@users.noreply.github.com>
2018-08-05 21:45:40 +0000

    Create consensus.py

---
+++ consensus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Copyright (c) 2018 CVSC
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+# One silme can be split into 100000000 satoshi
+COIN = 100000000
+# Proof of Work limit 
+bnProofOfWorkLimit = 0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+# Reward Per block
+nCoin = 50
+# Halving Every 210000 blocks
+nSubsibyHalvingInterval = 210000
+# MaxBlockSize
+nMaxSize = 1000000 # 1MB
+# coinbase maturity 7 blocks 
+COINBASE_MATURITY = 7
